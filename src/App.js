bb5d8ea82a5bd8d1d66dba688cf93fe90fa1ccfa
eunkeeee <eunkeeeee@gmail.com>
2022-08-29 00:38:13 +0900

    1.2 useInput part2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { useState } from "react";
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
-    console.log(event.target);
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { value, onChange };
 };
 function App() {
-  const name = useInput("Mr.");
+  const maxLen = (value) => value.includes("@");
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
       <h1>Hello</h1>
