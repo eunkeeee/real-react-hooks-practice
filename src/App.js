6a5feefc39041a33228594b02d98449aa20dd5b0
eunkeeee <eunkeeeee@gmail.com>
2022-08-29 15:31:52 +0900

    2.2 useRef

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
-};
 const App = () => {
-  const titleUpdater = useTitle("Loading...");
-  setTimeout(() => titleUpdater("Home"), 5000);
-  const sayHello = () => console.log("hello");
-  const [number, setNumber] = useState(0);
-  const [aNumber, setAnumber] = useState(0);
-  useEffect(() => sayHello(), [number]);
+  const potato = useRef();
+  setTimeout(() => console.log(potato.current.focus()), 5000);
   return (
     <div className="App">
       <div>Hi</div>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
+      <input ref={potato} placeholder="la" />
     </div>
   );
 };
