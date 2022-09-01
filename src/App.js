ffca665c9b074df7f2919da82c601ec528e6e55d
eunkeeee <eunkeeeee@gmail.com>
2022-09-01 15:36:10 +0900

    2.5 useFadeIn

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 
-const useBeforeLeave = (onBefore) => {
-  const handle = (event) => {
-    const { clientY } = event;
-    if (clientY <= 0) {
-      onBefore();
-    }
-  };
-
+const useFadeIn = (duration = 1, delay = 0) => {
+  const element = useRef();
   useEffect(() => {
-    document.addEventListener("mouseleave", handle);
-    return () => document.removeEventListener("mouseleave", handle);
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+
+      current.style.opacity = 1;
+    }
   }, []);
-  if (!onBefore || typeof onBefore !== "function") {
+  if (typeof duration !== "number" || typeof delay !== "number") {
     return;
   }
+  return { ref: element, style: { opacity: 0 } };
 };
+
 const App = () => {
-  const begForLife = () => console.log("Please don't leave...");
-  // 아무것도 return하지 않으므로 가능
-  useBeforeLeave(begForLife);
+  const fadeInH1 = useFadeIn(2, 0);
+  const fadeInP = useFadeIn(5, 1);
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 {...fadeInH1}>Hello</h1>
+      <p {...fadeInP}>hello</p>
     </div>
   );
 };
